75ebc880ceb008c5fef78c52e5d5543b90d86208
zhfkt <zhfkt@hotmail.com>
2016-03-04 21:51:12 +0800

    Test for git

--- src/main/java/com/tongmeng/txyspring/HomeController.java
+++ src/main/java/com/tongmeng/txyspring/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	
-	@JsonView(Views.Public.class)
+	//@JsonView(Views.Public.class)
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
