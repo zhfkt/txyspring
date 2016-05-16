e089e4530bdcf8b3c2ed14275fb4731c2d26234f
zhfkt <zhfkt@hotmail.com>
2016-05-16 18:34:25 +0800

    Add the tomcat service start on centos

--- install/install.sh
+++ install/install.sh
@@ -6,11 +6,13 @@ cd /root/develop/txyspring
 git pull
 
 service tomcat7 stop
+service tomcat stop
 
 cd /root/develop/txyspring/install
 mysql -uroot -ppassword -v -t < install.sql 
 
 service tomcat7 start
+service tomcat start 
 
 #sleep 10
 
