448de884282b5159edd170493d3cf9152952512e
zhfkt <zhfkt@hotmail.com>
2016-05-30 15:44:49 +0800

    add the more zhangwulu Campus

--- install/install_schema.sql
+++ install/install_schema.sql
@@ -144,7 +144,10 @@ insert sch_code(Area_Code,Description)
 values (10002,'同济大学四平路校区');
 
 insert sch_code(Area_Code,Description)
-values (10003,'同济大学沪西校区');
+values (10003,'同济大学彰武路校区');
+
+insert sch_code(Area_Code,Description)
+values (10004,'同济大学沪西校区');
 
 insert act_code(Act_Subtype,Description_Subtype)
 values (10000,'校园活动');
