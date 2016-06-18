544790dbd600d4a6dd2557caf9f096ac044c403a
zhfkt <zhfkt@hotmail.com>
2016-06-18 23:55:54 +0800

    Isolation.SERIALIZABLE in getUserAll to test the transcation Isolation of login

--- src/main/java/com/tongmeng/txyspring/service/UserService.java
+++ src/main/java/com/tongmeng/txyspring/service/UserService.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tongmeng.txyspring.ajaxmodel.ActInfoAjax;
@@ -109,7 +110,7 @@ public class UserService {
 
 
 	
-	@Transactional
+	@Transactional(isolation = Isolation.SERIALIZABLE)
 	public int getUserAll(IdentityInterface.UserIdentity userIdentity) {
 		
 		if(!userIdentity.ifLogin())
